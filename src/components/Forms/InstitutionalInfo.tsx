d57fab8c0508e92f00d2c0c8a6c015f6e2ac9ea0
jaybarnes33 <57744029+jaybarnes33@users.noreply.github.com>
2021-11-14 14:41:07 +0000

    Revert "Fix: Add campus field v2"

--- src/components/Forms/InstitutionalInfo.tsx
+++ src/components/Forms/InstitutionalInfo.tsx
@@ -17,20 +17,21 @@ const InstitutionalInfo = ({ formik }: { formik: TFormik }) => {
   return (
     <div className={styles.inner}>
       <div className={styles.input}>
-        <label htmlFor="campus">
-          campus <span>*</span>
+        <label htmlFor="hall">
+          Hall <span>*</span>
         </label>
+        <br />
         <select
-          id="campus"
-          name="campus"
-          className={styles.input}
+          id="hall"
           value={values.campus}
+          className={styles.input}
+          name="campus"
+          required
           onChange={handleChange}
           aria-describedby="campus-message"
-          required
         >
-          <option>Select campus</option>
-          {["Essikado", "Tarkwa"].map((campus, index) => (
+          <option>Select Campus</option>
+          {campuses.map((campus, index) => (
             <option value={campus} key={`campus-${index}`}>
               {campus}
             </option>
@@ -87,6 +88,30 @@ const InstitutionalInfo = ({ formik }: { formik: TFormik }) => {
         </select>
         {errors.level && touched.level && (
           <small id="level-message">{errors.level}</small>
+        )}
+      </div>
+      <div className={styles.input}>
+        <label htmlFor="campus">
+          campus <span>*</span>
+        </label>
+        <select
+          id="campus"
+          name="campus"
+          className={styles.input}
+          value={values.campus}
+          onChange={handleChange}
+          aria-describedby="campus-message"
+          required
+        >
+          <option>Select campus</option>
+          {["Esikado", "Tarkwa"].map((campus, index) => (
+            <option value={campus} key={`campus-${index}`}>
+              {campus}
+            </option>
+          ))}
+        </select>
+        {errors.campus && touched.campus && (
+          <small id="campus-message">{errors.campus}</small>
         )}
       </div>
     </div>
