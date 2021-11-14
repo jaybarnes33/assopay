9cc6490745e9550782b18c152c468f84e7699b2a
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2021-11-14 18:31:43 +0000

    FeatL add charges

--- src/utils/getAmount.ts
+++ src/utils/getAmount.ts
@@ -1,12 +1,12 @@
 export const getAmount = (level: number) => {
   switch (level) {
     case 100:
-      return 50;
+      return 52;
 
     case 200:
     case 300:
     case 400:
-      return 30;
+      return 32;
 
     default:
       return 0;
